2a91d6fe3c1e23c8c81e7256a4372c177534e14a
bixlogisticx <boji.220@gmail.com>
2013-05-08 22:54:52 -0400

    ruby on rails push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bizlogistix::Application.config.secret_token = 'ca860df458b55e54811e045517cdaed6276a0fe7d58b0dd1cfa4d8683073955a210afaf2777e487cf724be425a1bae4cfacf583c5fe475af667380b304238b91'
