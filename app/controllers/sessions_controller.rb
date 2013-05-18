cbb37ce48c5b30b3a1d03e861ddd9668cdb27d61
bixlogisticx <boji.220@gmail.com>
2013-05-18 15:45:36 -0400

    tried to push changes made by karan jut for test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,10 +12,11 @@ class SessionsController < ApplicationController
   	@user = User.find_by_email(params[:email])
 	if @user
 		session[:first] = @user.first
+		session[:last] = @user.last
 		session[:email] = @user.email
 		if @user.authenticate(params[:password])
 			session[:user_id] = @user.id
-			redirect_to '/users/my_account', notice: 'You are now logged in!'
+			redirect_to '/users/my_account', notice: "Welcome #{@user.first}!"
 		else
 			flash.now.alert = 'Incorrect password! Please try again.'
 			render 'new'
@@ -27,6 +28,7 @@ class SessionsController < ApplicationController
 
   def destroy
   	session[:first] = nil
+  	session[:last] = nil
   	session[:user_id] = nil
 	session[:email] = nil
 	redirect_to '/login', notice: 'You have logged out of your account!'
