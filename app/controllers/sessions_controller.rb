eda51f5dd65f3336bffdeb4b4d92d6d9a0ac2898
bixlogisticx <boji.220@gmail.com>
2013-05-18 16:37:03 -0400

    styling of the login page added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,11 +12,10 @@ class SessionsController < ApplicationController
   	@user = User.find_by_email(params[:email])
 	if @user
 		session[:first] = @user.first
-		session[:last] = @user.last
 		session[:email] = @user.email
 		if @user.authenticate(params[:password])
 			session[:user_id] = @user.id
-			redirect_to '/users/my_account', notice: "Welcome #{@user.first}!"
+			redirect_to '/users/my_account', notice: 'You are now logged in!'
 		else
 			flash.now.alert = 'Incorrect password! Please try again.'
 			render 'new'
@@ -28,7 +27,6 @@ class SessionsController < ApplicationController
 
   def destroy
   	session[:first] = nil
-  	session[:last] = nil
   	session[:user_id] = nil
 	session[:email] = nil
 	redirect_to '/login', notice: 'You have logged out of your account!'
