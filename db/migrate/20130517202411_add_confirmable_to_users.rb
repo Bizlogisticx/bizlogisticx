124ee50239b96f935f68784bcf3eb8ac86a95fcc
Karan Tamhane <karan.tamhane@gmail.com>
2013-05-23 09:50:53 -0400

    javascript include tags removed from forms jquery and jquery_ujs requires removed to solve multiple popup issue

---
+++ db/migrate/20130517202411_add_confirmable_to_users.rb
@@ -0,0 +1,18 @@
+class AddConfirmableToUsers < ActiveRecord::Migration
+  def up
+    add_column :users, :confirmation_token, :string
+    add_column :users, :confirmed_at, :datetime
+    add_column :users, :confirmation_sent_at, :datetime
+    add_column :users, :unconfirmed_email, :string
+    add_index :users, :confirmation_token, :unique =>  true
+    User.update_all(:confirmed_at => Time.now)
+  end
+
+  def down
+  	remove_index :users, :confirmation_token
+  	remove_column :users, :unconfirmed_email, :string
+  	remove_column :users, :confirmation_sent_at, :datetime
+  	remove_column :users, :confirmed_at, :datetime
+  	remove_column :users, :confirmation_token, :string
+  end
+end
